0b83a51a3cb4bc18b1e79ebf01e2bf5fb9597d7b
Naoise Golden <naoise.golden@xing.com>
2014-10-17 15:31:32 +0200

    fix geolocation.js

--- tinder.geolocation.js
+++ tinder.geolocation.js
@@ -1,4 +1,4 @@
-/** 
+/**
 * TinderGeolocation
 * A demonstration of the Constructor Pattern
 * to retrieve your current coordinates.
@@ -11,19 +11,21 @@
 */
 
 var TinderGeolocation = function() {
-  this.geolocate = function(callback) {
-    if (navigator.geolocation) {
+  var callback;
 
+  function onSuccess(position) {
+    callback(position.coords);
+  }
+
+  function onError() {
+    throw new Error("Could not get latitude and longitude");
+  }
+
+  this.geolocate = function(callbackFunction) {
+    callback = callbackFunction;
+    if (navigator.geolocation) {
       // get permission
       navigator.geolocation.getCurrentPosition(onSuccess, onError);
-      
-      function onSuccess(position) {
-        callback(position.coords);
-      }
-
-      function onError() {
-        throw new Error("Could not get latitude and longitude");
-      }
     }
-  }
-}
+  };
+};
